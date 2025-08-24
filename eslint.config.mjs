1647b424c5d450eecfb82f8c80042d063748910d
Kiran C <kiran.ck.work@gmail.com>
2025-08-24 15:45:14 +0530

    Adding perfectionist rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,6 +8,7 @@ import react from 'eslint-plugin-react';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
 import prettier from 'eslint-plugin-prettier';
 import prettierConfig from 'eslint-config-prettier';
+import perfectionist from 'eslint-plugin-perfectionist';
 
 export default [
   {
@@ -39,14 +40,16 @@ export default [
       '@typescript-eslint': tseslint,
       'jsx-a11y': jsxA11y,
       prettier,
+      perfectionist,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...tseslint.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      ...jsxA11y.configs.recommended.rules,
+      ...jsxA11y.configs.strict.rules,
       ...prettierConfig.rules,
+      ...perfectionist.configs['recommended-natural'].rules,
       
       // React specific rules
       'react/react-in-jsx-scope': 'off',
@@ -97,7 +100,7 @@ export default [
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      ...jsxA11y.configs.recommended.rules,
+      ...jsxA11y.configs.strict.rules,
       ...prettierConfig.rules,
       
       'react/react-in-jsx-scope': 'off',
